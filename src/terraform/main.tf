90f72546d425d8413d6cdab7a04021b8a1db9e4d
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-21 16:59:51 -0500

    m

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -42,3 +42,29 @@ tags = {
     environment = "Production"
   }
 }
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "mysecuritybt"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
