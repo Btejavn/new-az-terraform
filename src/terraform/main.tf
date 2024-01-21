5926c6d435c8e1d9c75698895b05f51e34a68018
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-21 16:33:01 -0500

    k

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -24,9 +24,50 @@ resource "azurerm_virtual_network" "vnet" {
   location            = var.location
   address_space       = ["10.0.0.0/16"]  # Customize the address space as needed
 }
- resource "azurerm_subnet" "subnet" {
-  name                 = "internal"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = ["10.0.2.0/24"]
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                        = "bhanukeyvault"
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
 }
