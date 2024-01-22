ba94047476fa7fb3fca1125f430105fcfab21806
Bhanu Teja Chittabathini <bchittabathini@MacBook-Pro-2.local>
2024-01-21 21:45:39 -0500

    d

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -135,7 +135,7 @@ data "azurerm_client_config" "current" {}
 
 
 resource "azurerm_key_vault" "keyvault" {
-  name                        = "my-keyvault"
+  name                        = "btkcmafwfw"
   location                    = var.location
   resource_group_name         = var.resource_group_name
   enabled_for_disk_encryption = true
